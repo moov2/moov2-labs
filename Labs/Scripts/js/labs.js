2c51f5bf42463822efc5cdda46e7180556af36bf
Lawrence Dine <lawrence@moov2.com>
2012-01-11 09:33:05 +0000

    Initial Commit: basic labs site and local storage demo

---
+++ Labs/Scripts/js/labs.js
@@ -0,0 +1,22 @@
+﻿﻿/**
+* Global namespace.
+*/
+var Labs = Labs || {};
+
+$(document).ready(function () {
+    Labs.localStorage.initialise();
+});
+
+/**
+* Returns true if supplied keycode is relevant for typing. Used with keyup events to prevent firing on shift, windows key, alt etc
+*/
+Labs.typingKeycode = function (keyCode) {
+    return (keyCode == 8 || keyCode == 13 || keyCode == 46 || (keyCode > 48 && keyCode < 90) || (keyCode > 96 && keyCode < 105))
+};
+
+var t;
+
+Labs.callbackTimeout = function (callback) {
+    clearTimeout(t);
+    t = setTimeout(callback, 500);
+};
---
+++ Labs/Scripts/js/labs.localStorage.js
@@ -0,0 +1,45 @@
+﻿Labs.localStorage = {};
+
+Labs.localStorage.localTextSelector = "#local-text";
+
+Labs.localStorage.localText;
+
+Labs.localStorage.initialise = function () {
+    if (Labs.localStorage.isLocalStoragePage()) {
+        Labs.localStorage.localText = $(Labs.localStorage.localTextSelector);
+        Labs.localStorage.localText.wysiwyg({ events: {
+                keyup: Labs.localStorage.wysiwygChanged
+            }
+        });
+        Labs.localStorage.loadWysiwyg();
+        Labs.localStorage.updateStatus();
+        setInterval(Labs.localStorage.updateStatus, 5000);
+    }
+}
+
+Labs.localStorage.isLocalStoragePage = function () {
+    return $(Labs.localStorage.localTextSelector).length > 0;
+}
+
+Labs.localStorage.updateStatus = function () {
+    var seconds = 0;
+    if (window.localStorage.getItem('value')) {
+        seconds = ((new Date()).getTime() - (new Date()).setTime(window.localStorage.getItem('timestamp'))) / 1000;
+        $("#local-storage-info").html('last saved: ' + seconds + 's ago');
+    }
+}
+
+Labs.localStorage.wysiwygChanged = function (e) {
+    Labs.localStorage.saveWysiwyg();
+}
+
+Labs.localStorage.saveWysiwyg = function () {
+    window.localStorage.setItem('wysiwyg-text', Labs.localStorage.localText.wysiwyg("getContent"));
+    window.localStorage.setItem('timestamp', (new Date()).getTime());
+}
+
+Labs.localStorage.loadWysiwyg = function () {
+    if (window.localStorage.getItem('wysiwyg-text')) {
+        Labs.localStorage.localText.wysiwyg("setContent", window.localStorage.getItem('wysiwyg-text'));
+    }
+}
