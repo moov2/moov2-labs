557f32a2ad21f288ced28e15447f12b0914ea26e
Peter Keating <peter@moov2.com>
2013-09-30 13:19:37 +0100

    updated the offline JS to include source code for the example.

--- Labs/Scripts/js/labs.offlineVideo.js
+++ Labs/Scripts/js/labs.offlineVideo.js
@@ -7,25 +7,60 @@ $(document).ready(function () {
 Labs.offlineVideo = function () {
 
     var blob;
+    var db = new PouchDB('offlinevideo');
 
-    var init = function () {
+    var addVideoToPage = function (video) {
+        $('.js-video-container').html('<video class="js-video" style="width: 320px; height: 240px;" controls></video>');
+        $('.js-video-container video').attr('src', window.URL.createObjectURL(video));
+    };
+
+    var getVideoFromLocalDB = function () {
+        db.getAttachment('offline-video', '/video', function(err, res) {
+            if (res) {
+                addVideoToPage(res);
+                return;
+            }
+
+            getVideoFromServer();
+        });
+    };
+
+    var getVideoFromServer = function () {
         var xhr = new XMLHttpRequest();
 
-         xhr.open("GET", "/videos/offline-video-example.mp4", true);
-         xhr.responseType = "blob";
-         xhr.addEventListener("load", function () {
+        xhr.open("GET", "/videos/offline-video-example.mp4", true);
+        xhr.responseType = "blob";
+        xhr.addEventListener("load", function () {
             if (xhr.status !== 200) {
                 return;
             }
 
             blob = xhr.response;
-            console.log('success');
+            saveVideoToLocalDB(blob);
         }, false);
 
         xhr.send();
     };
 
+    var init = function () {
+        getVideoFromLocalDB();
+
+        $('.js-clear').on('click', function () {
+            PouchDB.destroy('offlinevideo');
+        });
+
+        return;
+    };
+
+    saveVideoToLocalDB = function (blob) {
+        db.put({_id: 'offline-video'}, function(err, response) {
+            db.putAttachment(response.id, '/video', response.rev, blob, 'video/mp4', function (err, res) {
+                addVideoToPage(blob);
+            });
+        });
+    };
+
     return {
         init: init
-    }
-});
+    };
+}();
